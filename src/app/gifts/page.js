ef21fcb34213795574245cd9f3970c914feffe97
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-02-19 08:38:02 -0600

    form ui complete with routes

--- src/app/gifts/page.js
+++ src/app/gifts/page.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import Link from 'next/link';
 import GiftCard from '../../components/cards/GiftCard';
 
 export default function GiftsPage() {
-  return <GiftCard />;
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <Link href="/gift/new" passHref>
+          <Button>New Gift</Button>
+        </Link>
+      </div>
+
+      <GiftCard />
+    </>
+  );
 }
