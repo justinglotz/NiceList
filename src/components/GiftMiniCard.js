e4ffbae834fef23de37aaddbb0aa3c3ccf4a502b
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-02-22 09:53:57 -0600

    gift mini cards on dashboard cards read gift name

--- src/components/GiftMiniCard.js
+++ src/components/GiftMiniCard.js
@@ -1,23 +1,33 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function GiftMiniCard() {
+export default function GiftMiniCard({ giftObj }) {
   return (
     <div className="h-[50px] w-[285px]">
-        <div className="h-1/2 bg-[#E6DADA] rounded-t-[6px]" />
-        <div className="h-[25px] bg-[#C25B5D] rounded-b-[6px] flex flex-row divide-x divide-solid divide-black">
-          <div className="flex h-full w-1/4 justify-center">
-            <p className="text-white text-[12px] my-1">Selected</p>
-          </div>
-          <div className="flex w-1/4 justify-center h-full">
-            <p className="text-white text-[12px] my-1">Purchased</p>
-          </div>
-          <div className="flex w-1/4 justify-center h-full">
-            <p className="text-white text-[12px] my-1">Arrived</p>
-          </div>
-          <div className="flex w-1/4 justify-center h-full">
-            <p className="text-white text-[12px] my-1">Complete</p>
-          </div>
+      <div className="h-[25px] bg-[#E6DADA] rounded-t-[6px]">
+        <p>{giftObj.name}</p>
+      </div>
+      <div className="h-[25px] bg-[#C25B5D] rounded-b-[6px] flex flex-row divide-x divide-solid divide-black">
+        <div className="flex h-full w-1/4 justify-center">
+          <p className="text-white text-[12px] my-1">Selected</p>
+        </div>
+        <div className="flex w-1/4 justify-center h-full">
+          <p className="text-white text-[12px] my-1">Purchased</p>
+        </div>
+        <div className="flex w-1/4 justify-center h-full">
+          <p className="text-white text-[12px] my-1">Arrived</p>
+        </div>
+        <div className="flex w-1/4 justify-center h-full">
+          <p className="text-white text-[12px] my-1">Complete</p>
         </div>
       </div>
+    </div>
   );
 }
+
+GiftMiniCard.propTypes = {
+  giftObj: PropTypes.shape({
+    name: PropTypes.string,
+    giftId: PropTypes.string,
+  }),
+};
