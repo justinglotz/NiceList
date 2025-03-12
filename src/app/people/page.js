414a0b84b0cdf5702bbf071a0b3f2574148e6de6
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-03-12 07:32:36 -0500

    styled person cards

--- src/app/people/page.js
+++ src/app/people/page.js
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState, useEffect, useMemo } from 'react';
-import { Button } from 'react-bootstrap';
 import Link from 'next/link';
 import PersonCard from '../../components/cards/personCard';
 import { getPeople } from '../../api/personData';
@@ -10,11 +9,20 @@ import { useSearch } from '../../utils/context/searchContext';
 
 export default function PeoplePage() {
   const [people, setPeople] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { user } = useAuth();
   const { searchQuery } = useSearch();
 
   const getAllThePeople = () => {
-    getPeople(user.uid).then(setPeople);
+    setLoading(true);
+    getPeople(user.uid).then((data) => {
+      setPeople(data);
+      setTimeout(() => setLoading(false), 900);
+    });
+  };
+
+  const handleDeletePerson = (deletedPersonId) => {
+    setPeople((prevPeople) => prevPeople.filter((person) => person.personId !== deletedPersonId));
   };
 
   useEffect(() => {
@@ -28,28 +36,28 @@ export default function PeoplePage() {
       return people;
     }
 
-    // Filter ideas that match the search query
+    // Filter people that match the search query
     return people.filter((person) => {
       const searchLower = searchQuery.toLowerCase();
-      // Adjust these fields based on your gift idea structure
       return person.name?.toLowerCase().includes(searchLower) || person.address?.toLowerCase().includes(searchLower);
     });
   }, [people, searchQuery]);
 
   return (
-    <div>
-      <div className="flex justify-center items-center">
+    <>
+      <div className="flex justify-center items-center flex-col mt-[15px]">
+        <h3 className="font-quicksand">People</h3>
         <Link href="/person/new" passHref>
-          <Button>New Person</Button>
+          <button type="button" className="px-4 py-2 text-lg bg-[#7fa087] hover:bg-[#6b8872] text-black rounded">
+            New Person
+          </button>
         </Link>
       </div>
-      <div className="mx-auto w-4/5">
-        <div className="flex flex-row flex-wrap justify-start gap-3 m-2">
-          {filteredPeople.map((person) => (
-            <PersonCard key={person.personId} personObj={person} onUpdate={getAllThePeople} />
-          ))}
-        </div>
+      <div className="flex flex-row flex-wrap justify-center gap-4 px-8 mb-4 mt-2">
+        {filteredPeople.map((person) => (
+          <PersonCard key={person.personId} personObj={person} onUpdate={handleDeletePerson} loading={loading} />
+        ))}
       </div>
-    </div>
+    </>
   );
 }
